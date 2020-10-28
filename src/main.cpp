cae389fbfbef56366b252b0f8fcf5d9452797fd1
Thomas Malt <thomas@malt.no>
2020-10-28 21:19:32 +0100

    empty script

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,13 @@
 
 #include <Arduino.h>
-#include <FastLED.h>
-//     -DLED_TYPE="SK9822"
-//     -DLED_COUNT=140
-//     -DLED_DATA=17
-//     -DLED_CLOCK=16
-//     -DLED_COLOR_ORDER="BGR"
-//     -DLED_mA=12000
-//     -DFFT_ACTIVE=1
-//     -DFFT_INPUT_PIN=34
-//     -DFPS=120
-#define NUM_LEDS 30
-#define DATA_PIN 17
-#define CLOCK_PIN 16
 
-CRGB leds[NUM_LEDS];
-
-void setup() { Serial.begin(115200); }
+void setup() {
+  // Initialize Serial port
+  Serial.begin(115200);
+}
 
 void loop() {
+  // The almost simplest possible loop
   Serial.println("Hello World.");
   delay(1000);
 }
