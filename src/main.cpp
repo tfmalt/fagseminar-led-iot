c652200e54f80e970a8692446ea59f788dab21a0
Thomas Malt <thomas@malt.no>
2020-10-28 21:27:37 +0100

    blink

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,30 @@
 
 #include <Arduino.h>
+#include <FastLED.h>
+
+#define NUM_LEDS 30
+#define DATA_PIN 17
+#define CLOCK_PIN 16
+
+CRGB leds[NUM_LEDS];
 
 void setup() {
   // Initialize Serial port
   Serial.begin(115200);
+
+  // FastLED setup
+  FastLED.addLeds<SK9822, DATA_PIN, CLOCK_PIN, BGR>(leds, NUM_LEDS);
 }
 
 void loop() {
   // The almost simplest possible loop
   Serial.println("Hello World.");
-  delay(1000);
+
+  leds[9] = CRGB::Red;
+  FastLED.show();
+  delay(500);
+
+  leds[9] = CRGB::Black;
+  FastLED.show();
+  delay(500);
 }
